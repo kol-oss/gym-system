beae0a29cd96dd24f1b964c3a4c688e4c05fb350
kol-oss <kolyakhozhajinov@gmail.com>
2025-09-28 23:47:52 +0300

    Add usages example to entry point

--- src/main/java/com/epam/gym/Main.java
+++ src/main/java/com/epam/gym/Main.java
@@ -1,39 +1,132 @@
 package com.epam.gym;
 
 import com.epam.gym.config.AppConfig;
-import com.epam.gym.controller.GymController;
+import com.epam.gym.controller.*;
+import com.epam.gym.dto.*;
 import com.epam.gym.model.Trainee;
 import com.epam.gym.model.Trainer;
 import com.epam.gym.model.Training;
+import com.epam.gym.model.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) {
         try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class)) {
-            GymController gymController = context.getBean(GymController.class);
+            EntityManagerFactory entityManagerFactory = context.getBean(EntityManagerFactory.class);
+            EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-            Trainee trainee = new Trainee();
-            trainee.setFirstName("John");
-            trainee.setLastName("Doe");
+            AuthController authController = context.getBean(AuthController.class);
+            UserController userController = context.getBean(UserController.class);
 
-            // John.Doe and John.Doe1
-            gymController.addTrainee(trainee);
-            gymController.addTrainee(trainee);
+            TrainerController trainerController = context.getBean(TrainerController.class);
+            TraineeController traineeController = context.getBean(TraineeController.class);
+            TrainingController trainingController = context.getBean(TrainingController.class);
 
-            Trainer trainer = new Trainer();
-            trainer.setFirstName("Alan");
-            trainer.setLastName("Doe");
+            // 0. User creation
+            CreateUserDto userDto = new CreateUserDto();
+            userDto.setFirstName("John");
+            userDto.setLastName("Doe");
 
-            // Alan.Doe and Alan.Doe1
-            gymController.addTrainer(trainer);
-            gymController.addTrainer(trainer);
+            CreateUserResponse response = userController.post(userDto);
+            String username = response.getUsername();
+            String password = response.getPassword();
 
-            Training training = new Training();
-            training.setName("Training");
-            training.setTraineeId(trainee.getId());
-            training.setTrainerId(trainer.getId());
+            // 0. User retrieval
+            // String username = "John.Doe";
+            // String password = "h0G&?8!eOr";
 
-            gymController.addTraining(training);
+            User user = userController.getByUsername(username);
+
+            // 1. Trainer creation
+            UUID trainingTypeId = UUID.fromString("a1b3c5d8-214a-4894-8000-639d0178d20d");
+
+            CreateTrainerDto trainerDto = new CreateTrainerDto();
+            trainerDto.setUserId(user.getId());
+            trainerDto.setSpecializationId(trainingTypeId);
+
+            trainerController.post(trainerDto);
+
+            // 2. Trainee creation
+            CreateTraineeDto traineeDto = new CreateTraineeDto();
+            traineeDto.setUserId(user.getId());
+            traineeDto.setAddress("Kyiv, st. 5");
+            traineeDto.setBirthDate(LocalDate.now());
+
+            traineeController.post(traineeDto);
+
+            // 3 & 4. User login
+            String token = authController.login(username, password);
+
+            // 5. Select Trainer profile by username.
+            Trainer trainer = trainerController.getByUsername(username);
+
+            // 6. Select Trainee profile by username.
+            Trainee trainee = traineeController.getByUsername(username);
+
+            // 7 & 8. Password change.
+            password = "12345";
+            userController.patchPassword(user.getId(), password, token);
+
+            // 9 & 10. Update trainer/trainee profile.
+            UpdateTraineeDto updateTraineeDto = new UpdateTraineeDto();
+            updateTraineeDto.setAddress("Lviv, st. 10");
+
+            traineeController.put(trainee.getId(), updateTraineeDto, token);
+
+            // 11 & 12. Activate/De-activate trainer/trainee.
+            userController.patchStatus(user.getId(), false, token);
+
+            // 13. Delete trainee profile by username.
+            userController.deleteByUsername(username, token);
+
+            // 14 & 15. Get Trainings List by filters.
+            CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+            CriteriaQuery<Training> filters = cb.createQuery(Training.class);
+            Root<Training> root = filters.from(Training.class);
+
+            List<Predicate> predicates = List.of(
+                    cb.equal(root.get("trainee").get("username"), username),
+                    cb.greaterThanOrEqualTo(root.get("date"), LocalDate.of(2025, 1, 1)),
+                    cb.lessThanOrEqualTo(root.get("date"), LocalDate.of(2025, 12, 11)),
+                    cb.equal(root.get("trainer").get("name"), username)
+            );
+
+            filters.select(root)
+                    .where(cb.and(predicates.toArray(Predicate[]::new)))
+                    .orderBy(cb.asc(root.get("date")));
+
+            trainingController.getAll(filters, token);
+
+            // 16. Add training.
+            CreateTrainingDto trainingDto = new CreateTrainingDto();
+            trainingDto.setName("Regular training");
+            trainingDto.setDuration(5);
+            trainingDto.setTrainingTypeId(trainingTypeId);
+            trainingDto.setTrainerId(trainer.getId());
+            trainingDto.setTraineeId(trainee.getId());
+
+            trainingController.post(trainingDto, token);
+
+            // 17. Get trainers list that not assigned on trainee by trainee's username.
+            List<Trainer> freeTrainers = trainerController.getFreeTrainersByTraineeUsername(username, token);
+
+            // 18. Update Tranee's trainers list
+            List<UUID> trainersForTrainee = List.of(trainer.getId());
+
+            UpdateTraineeDto updateTrainersDto = new UpdateTraineeDto();
+            updateTrainersDto.setTrainerIds(trainersForTrainee);
+
+            traineeController.put(trainee.getId(), updateTraineeDto, token);
         }
     }
 }
